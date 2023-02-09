ce7b3ffe1e3cec44d7ed373011756bd8aa073b6d
evertonrivas <evertonrivas@gmail.com>
2023-02-09 15:49:23 -0300

    Adicionado nome do modulo

--- b2b/api.py
+++ b2b/api.py
@@ -6,7 +6,7 @@ blueprint = Blueprint("b2b",__name__,url_prefix="/b2b/api/v1/")
 api = Api(blueprint,
     version="1.0",
     title="API Venda Fashion",
-    description="Uma API REST para sistema de vendas",
+    description="Uma API REST para sistema de vendas - Módulo B2B",
     contact_email="evertonrivas@gmail.com",
     contact="Venda",
     contact_url="http://www.vendafashion.com")
