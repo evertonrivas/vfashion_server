91f80d8650daa310caeda017ccbd22c3c061b994
evertonrivas <evertonrivas@gmail.com>
2023-02-09 17:46:39 -0300

    Documentação

--- crm/api.py
+++ crm/api.py
@@ -2,6 +2,12 @@ from flask import Blueprint
 from flask_restx import Api
 from crm.opportunities import api as ns_opportunity
 
+""" Módulo Customer Relationship Management (Gestão de Relacionamento com o Cliente). 
+    Módulo para gestão de contatos com o cliente incluindo vida financeira (emissão de boletos, 2ª via NF-e, qualificação)
+
+Keyword arguments: relacionamento, clientes, contatos, financeiro
+"""
+
 nss = [ns_opportunity]
 
 blueprint = Blueprint("crm",__name__,url_prefix="/crm/api/v1/")
