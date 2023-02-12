34728c1444072015b324602cd635e84ac2742cf0
evertonrivas <evertonrivas@gmail.com>
2023-02-12 20:28:57 -0300

    Adicionados customer e customer_group

--- cmm/api.py
+++ cmm/api.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 from flask_restx import Api
 from cmm.users import api as ns_user
 from cmm.products import api as ns_product
+from cmm.customers import api as ns_customer
+from cmm.customers import apis as ns_group_group
 
 """ Módulo Common entre os sistemas
     Módulo que unificará os sistemas através de uma única API com coisas que são comuns em todas as etapas
@@ -10,7 +12,7 @@ Keyword arguments: usuarios, produtos
 """
 
 
-nss = [ns_user,ns_product]
+nss = [ns_user,ns_product,ns_customer,ns_group_group]
 
 blueprint = Blueprint("cmm",__name__,url_prefix="/cmm/api/v1/")
 
