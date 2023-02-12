5958f532323d451bc3d429e9eea818aa2d25ab3c
evertonrivas <evertonrivas@gmail.com>
2023-02-12 20:26:45 -0300

    removidos

--- crm/opportunities.py
+++
@@ -1,5 +0,0 @@
-from http import HTTPStatus
-from typing import TypedDict
-from flask_restx import Resource,fields,reqparse,Namespace
-
-api = Namespace("customers",description="Operações para manipular dados de clientes")
