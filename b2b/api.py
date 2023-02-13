80f7de11216fa7d0f93ab6a505b40e94cd60c5d4
evertonrivas <evertonrivas@gmail.com>
2023-02-13 08:38:03 -0300

    Nome do sistema

--- b2b/api.py
+++ b2b/api.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
 from flask_restx import Api
+from b2b.cart import api as ns_cart
+
+
+nss = [ns_cart]
 
 blueprint = Blueprint("b2b",__name__,url_prefix="/b2b/api/v1/")
 
@@ -8,5 +12,8 @@ api = Api(blueprint,
     title="API Venda Fashion",
     description="Uma API REST para sistema de vendas - Módulo B2B",
     contact_email="evertonrivas@gmail.com",
-    contact="Venda",
+    contact="Venda Fashion",
     contact_url="http://www.vendafashion.com")
+
+for ns in nss:
+    api.add_namespace(ns)
