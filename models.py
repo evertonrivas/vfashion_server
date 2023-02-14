1119843727a45f2efd30a72033f4b86182fae929
evertonrivas <evertonrivas@gmail.com>
2023-02-14 17:32:45 -0300

    Novas tabelas adicionadas

--- models.py
+++ models.py
@@ -1,18 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
 
 db = SQLAlchemy()
 
 
-""""id":1,
-"username": "teste",
-"password": "bolinha",
-"name": "Jose",
-"type": "A"""""
-
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    name     = db.Column(db.String(255), nullable=False)
-    type     = db.Column(db.CHAR,nullable=False)
+    id       = sa.Column("id",sa.Integer,primary_key=True,nullable=False,autoincrement=True)
+    username = sa.Column("username",sa.String(100), nullable=False)
+    password = sa.Column(sa.String(255), nullable=False)
+    name     = sa.Column(sa.String(255), nullable=False)
+    type     = sa.Column(sa.CHAR(1),nullable=False,default='L',comment='A = Administrador, L = Lojista, R = Representante')
+
+class Product(db.Model):
+    __tablename__ = "products"
+    id       = sa.Column("id",sa.Integer,primary_key=True,nullable=False,autoincrement=True)
+    prodCode = sa.Column("prodCode",sa.String(50),nullable=False)
+    barCode  = sa.Column("barCode",sa.String(128))
+    refCode  = sa.Column("refCode",sa.String(50),nullable=False)
+    name     = sa.Column("name",sa.String(255),nullable=False)
+    description = sa.Column("description",sa.String(255))
+    observation = sa.Column("objservation",sa.Text)
+    ncm         = sa.Column("ncm",sa.String(50))
+    image       = sa.Column("image",sa.String(500))
+    price       = sa.Column("price",sa.Float,nullable=False)
+    price_pdv   = sa.Column("price_pdv",sa.Float,nullable=True)
+    measure_unit = sa.Column("measure_unit",sa.CHAR(2),nullable=False)
+    structure    = sa.Column("structure",sa.CHAR(1),nullable=False,default='S',comment="S = Simples, C = Composto")
+
+class ProductSku(db.Model):
+    __tablename__ = "products_sku"
+    id_product = sa.Column("id_product",sa.Integer,primary_key=True,nullable=False)
+    color      = sa.Column("color",sa.String(10),primary_key=True,nullable=False)
+    size       = sa.Column("size",sa.String(5),primary_key=True,nullable=False)
+    quantity   = sa.Column("quantity",sa.Integer)
