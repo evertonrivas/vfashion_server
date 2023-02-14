552df42bb6376c3dd36afeabbe67883d10b4e86a
evertonrivas <evertonrivas@gmail.com>
2023-02-14 11:19:34 -0300

    Pré-definição

--- b2b/orders.py
+++ b2b/orders.py
@@ -1,26 +1,37 @@
 from http import HTTPStatus
 from typing import TypedDict
-from flask_restx import Resource,fields,reqparse,Namespace
+from flask_restx import Resource,fields,Namespace
+from flask import request
 
 api = Namespace("orders",description="Operações para manipular dados de pedidos")
 
 #API Models
-order_model = api.model(
-    "Order",{
-        "id": fields.Integer,
-        "idcliente": fields.String,
-        "password": fields.String,
-        "name": fields.String,
-        "type": fields.String
+prod_order = api.model(
+    "Product",{
+        "idproduct": fields.Integer,
+        "quantity": fields.Integer
     }
 )
 
+order_model = api.model(
+    "Order",{
+        "id": fields.Integer,
+        "date_created": fields.Integer,
+        "idcustomer": fields.Integer,
+        "products": fields.List(fields.Nested(prod_order)),
+        "make_online": fields.Boolean,
+        "idpayment_condition": fields.Integer
+    }
+)
+
+class ProdOrder(TypedDict):
+    idproduct:int
+    quantity:float
+
 class Order(TypedDict):
     id:int
-    username:str
-    password:str
-    name:str
-    type:str
+    idcustomer:str
+    products:list[ProdOrder]
 
 
 ####################################################################################
@@ -33,19 +44,24 @@ class OrdersList(Resource):
     @api.param("page","Número da página de registros","query",type=int)
     @api.doc(description="Teste de documentacao")
     def get(self)->list[Order]:
-
+        
         return [{
-            "id":1,
-            "username": "teste",
-            "password": "bolinha",
-            "name": "Jose",
-            "type": "A"
+            "id": int(request.args.get("page")),
+            "idcustomer": "1",
+            "products": [{
+                "idproduct": 1,
+                "quantity": 10
+            }]
         }]
 
     @api.doc(parser=order_model)
     @api.response(HTTPStatus.OK.value,"Cria um novo pedido")
     @api.response(HTTPStatus.BAD_REQUEST.value,"Falha ao criar pedido!")
     def post(self)->int:
+        idcustomer = request.form.get("customer")
+        if (type(request.form.get("products")) and len(request.form.get("products")) > 0):
+            for product in request.form.get("products"):
+                print(product)
         return 0
 
 
@@ -56,7 +72,14 @@ class OrderApi(Resource):
     @api.response(HTTPStatus.OK.value,"Obtem um registro de pedido",order_model)
     @api.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
     def get(self,id:int)->Order:
-        return None
+        return {
+            "id": id,
+            "idcustomer": "1",
+            "products": [{
+                "idproduct": 1,
+                "quantity": 10
+            }]
+        }
 
     @api.doc(parser=order_model)
     @api.response(HTTPStatus.OK.value,"Salva dados de um pedido")
