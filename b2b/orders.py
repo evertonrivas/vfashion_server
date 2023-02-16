a12915f26931b5a84bf1bcd63bcc9d03f60fa3fd
evertonrivas <evertonrivas@gmail.com>
2023-02-16 14:53:40 -0300

    Criada toda a estrutura de pedido

--- b2b/orders.py
+++ b2b/orders.py
@@ -1,58 +1,11 @@
 from http import HTTPStatus
-from typing import TypedDict
-from flask_restx import Resource,fields,Namespace
+from flask_restx import Resource,Namespace
 from flask import request
-from models import B2bOrders,db
+from models import B2bOrders,B2bOrdersProducts,db
+from sqlalchemy import exc
 
 ns_order = Namespace("orders",description="Operações para manipular dados de pedidos")
-
-#API Models
-pag_model = ns_order.model(
-    "Pagination",{
-        "registers": fields.Integer,
-        "page": fields.Integer,
-        "per_page": fields.Integer,
-        "pages": fields.Integer,
-        "has_next": fields.Boolean
-    }
-)
-
-prod_order = ns_order.model(
-    "Product",{
-        "id_product": fields.Integer,
-        "quantity": fields.Integer
-    }
-)
-
-order_model = ns_order.model(
-    "Order",{
-        "id": fields.Integer,
-        "id_customer": fields.Integer,
-        "make_online": fields.Boolean,
-        "id_payment_condition": fields.Integer,
-        "date_created": fields.DateTime,
-        "date_updated": fields.DateTime,
-        "products": fields.List(fields.Nested(prod_order))
-    }
-)
-
-list_order_model = ns_order.model(
-    "Return",{
-        "pagination": fields.Nested(pag_model),
-        "data": fields.List(fields.Nested(order_model))
-    }
-)
-
-class ProdOrder(TypedDict):
-    id_product:int
-    quantity:float
-
-class Order(TypedDict):
-    id:int
-    id_customer:int
-    make_online:bool
-    id_payment_condition:int
-    products:list[ProdOrder]
+ns_porder = Namespace("orders-products",description="Operações para manipular dados de produtos de pedidos")
 
 
 ####################################################################################
@@ -60,7 +13,7 @@ class Order(TypedDict):
 ####################################################################################
 @ns_order.route("/")
 class OrdersList(Resource):
-    @ns_order.response(HTTPStatus.OK.value,"Obtem a listagem de pedidos",list_order_model)
+    @ns_order.response(HTTPStatus.OK.value,"Obtem a listagem de pedidos")
     @ns_order.response(HTTPStatus.BAD_REQUEST.value,"Falha ao listar registros!")
     @ns_order.param("page","Número da página de registros","query",type=int,required=True)
     def get(self):
@@ -80,8 +33,7 @@ class OrdersList(Resource):
                 "make_online": m.make_online,
                 "id_payment_condition": m.id_payment_condition,
                 "date_created": m.date_created.strftime("%Y-%m-%d %H:%M:%S"),
-                "date_updated": m.date_updated.strftime("%Y-%m-%d %H:%M:%S"),
-                "produtcts": []
+                "date_updated": m.date_updated.strftime("%Y-%m-%d %H:%M:%S")
             } for m in rquery.items]
         }
 
@@ -107,9 +59,9 @@ class OrdersList(Resource):
 @ns_order.param("id","Id do registro")
 class OrderApi(Resource):
 
-    @ns_order.response(HTTPStatus.OK.value,"Obtem um registro de pedido",order_model)
+    @ns_order.response(HTTPStatus.OK.value,"Obtem um registro de pedido")
     @ns_order.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
-    def get(self,id:int)->Order:
+    def get(self,id:int):
         return B2bOrders.query.get(id).to_dict()
 
     @ns_order.response(HTTPStatus.OK.value,"Salva dados de um pedido")
@@ -136,6 +88,46 @@ class OrderApi(Resource):
             order = B2bOrders.query.get(id)
             order.trash = True
             db.session.add(order)
+            db.session.commit()
+            return True
+        except:
+            return False
+
+
+@ns_porder.route("/")
+class ProductsOrderList(Resource):
+    @ns_porder.response(HTTPStatus.OK.value,"Obtem a listagem de produtos de pedidos")
+    @ns_porder.response(HTTPStatus.BAD_REQUEST.value,"Falha ao listar registros!")
+    @ns_porder.param("id_order","Número do pedido","query",type=int,required=True)
+    def get(self):
+        try:
+            rquery = B2bOrdersProducts.query.filter_by(id_order=int(request.args.get("id_order"))).all()
+            return [{
+                "id_product": m.id_product,
+                "color": m.color,
+                "size" : m.size,
+                "quantity": m.quantity
+            }for m in rquery]
+            
+        except exc.SQLAlchemyError as e:
+            raise e
+
+    @ns_porder.response(HTTPStatus.OK.value,"Registra produto em um pedido")
+    @ns_porder.response(HTTPStatus.BAD_REQUEST.value,"Falha ao associar produto ao pedido!")
+    @ns_porder.param("id_order","Id do pedido","formData",type=int,required=True)
+    @ns_porder.param("id_product","Id do Produto","formData",type=int,required=True)
+    @ns_porder.param("color","Codigo ou nome da cor (hexa ou ingles)","formData",required=True)
+    @ns_porder.param("size","Tamanho do produto","formData",required=True)
+    @ns_porder.param("quantity","Quantidade do produto","formData",type=int,required=True)
+    def post(self):
+        try:
+            pOrder = B2bOrdersProducts()
+            pOrder.id_order   = int(request.form.get("id_order"))
+            pOrder.id_product = int(request.form.get("id_product"))
+            pOrder.color = request.form.get("color")
+            pOrder.size  = request.form.get("size")
+            pOrder.quantity = int(request.form.get("quantity"))
+            db.session.add(pOrder)
             db.session.commit()
             return True
         except:
