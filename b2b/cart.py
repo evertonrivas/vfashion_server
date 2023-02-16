7a9e32f4079e48184ddcf9705a1dfacb040d2227
evertonrivas <evertonrivas@gmail.com>
2023-02-16 09:14:09 -0300

    Troca do api pelo namespace

--- b2b/cart.py
+++ b2b/cart.py
@@ -2,4 +2,4 @@ from http import HTTPStatus
 from typing import TypedDict
 from flask_restx import Resource,fields,Namespace
 
-api = Namespace("cart",description="Operações para manipular dados de pedidos de compras (carrinho)")
+ns_cart = Namespace("cart",description="Operações para manipular dados de pedidos de compras (carrinho)")
