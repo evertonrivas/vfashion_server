cabfef959fd9bf5a560031f7f7df000508223a07
evertonrivas <evertonrivas@gmail.com>
2023-02-23 22:08:04 -0300

    Adição do Auth

--- pdv/consumer.py
+++ pdv/consumer.py
@@ -1,6 +1,8 @@
 from http import HTTPStatus
 from typing import TypedDict
-from flask_restx import Resource,fields,reqparse,Namespace
+from flask_restx import Resource,fields,Namespace
+from flask import request
+from auth import auth
 
 api = Namespace("consumer",description="Operações para manipular dados de consumidores")
 apis = Namespace("consumer-groups",description="Operações para manipular grupos de consumidores")
@@ -45,6 +47,7 @@ class ConsumerList(Resource):
     @api.response(HTTPStatus.OK.value,"Obtem a listagem de consumidores",[cons_model])
     @api.response(HTTPStatus.BAD_REQUEST.value,"")
     @api.param("page","Número da página de registros","query",type=int,required=True)
+    #@auth.login_required
     def get(self)-> list[Consumer]:
 
         return [{
@@ -58,6 +61,7 @@ class ConsumerList(Resource):
     @api.doc(parser=cons_model)
     @api.response(HTTPStatus.OK.value,"Cria um novo consumidor")
     @api.response(HTTPStatus.BAD_REQUEST.value,"Falha ao criar consumidor!")
+    #@auth.login_required
     def post(self)->int:
         return 0
 
@@ -68,55 +72,18 @@ class CustomerApi(Resource):
 
     @api.response(HTTPStatus.OK.value,"Obtem um registro de um consumidor",cons_model)
     @api.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
+    #@auth.login_required
     def get(self,id:int)->Consumer:
         return None
 
     @api.response(HTTPStatus.OK.value,"Salva dados de um consumidor")
     @api.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
+    #@auth.login_required
     def post(self,id:int)->bool:
         return False
     
     @api.response(HTTPStatus.OK.value,"Exclui os dados de um consumidor")
     @api.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
-    def delete(self,id:int)->bool:
-        return False
-
-
-####################################################################################
-#            INICIO DAS CLASSES QUE IRAO TRATAR OS GRUPOS DE CONSUMIDORES          #
-####################################################################################
-@apis.route("/")
-class UserGroupsApi(Resource):
-    @api.response(HTTPStatus.OK.value,"Obtem um registro de um grupo de consumidores",[group_model])
-    @api.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
-    @api.param("page","Número da página de registros","query",type=int,required=True)
-    def get(self)->list[ConsumerGroup]:
-
-        return None
-
-    @api.doc(parser=group_model)
-    @api.response(HTTPStatus.OK.value,"Cria um novo grupo de consumidores")
-    @api.response(HTTPStatus.BAD_REQUEST.value,"Falha ao criar grupo de consumidores!")
-    def post(self)->int:
-
-        return 0
-
-
-@apis.route("/<int:id>")
-@apis.param("id","Id do registro")
-class UserGroupApi(Resource):
-    @apis.response(HTTPStatus.OK.value,"Salva dados de um grupo de consumidores",group_model)
-    @apis.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
-    def get(self,id:int)->ConsumerGroup:
-
-        return None
-    
-    @apis.response(HTTPStatus.OK.value,"Salva dados de um grupo de consumidores")
-    @apis.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
-    def post(self,id:int)->bool:
-        return False
-    
-    @apis.response(HTTPStatus.OK.value,"Exclui os dados de um grupo de consumidores")
-    @apis.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
+    #@auth.login_required
     def delete(self,id:int)->bool:
         return False
