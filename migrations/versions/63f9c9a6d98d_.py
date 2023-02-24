ade1e38008a1056183c5884576eebb355be81636
evertonrivas <evertonrivas@gmail.com>
2023-02-24 09:14:47 -0300

    Migrations

---
+++ migrations/versions/63f9c9a6d98d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 63f9c9a6d98d
+Revises: 110e6037af69, 3fa5fd63142f
+Create Date: 2023-02-24 08:34:14.286546
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '63f9c9a6d98d'
+down_revision = ('110e6037af69', '3fa5fd63142f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/b91a133ebea1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b91a133ebea1
+Revises: 63f9c9a6d98d
+Create Date: 2023-02-24 08:36:28.173833
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b91a133ebea1'
+down_revision = '63f9c9a6d98d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
