6ff9917d216177a1c36bf0c109b1ef2223fb629d
evertonrivas <evertonrivas@gmail.com>
2023-02-23 23:56:15 -0300

    Melhorias

--- cmm/users.py
+++ cmm/users.py
@@ -3,6 +3,7 @@ from flask_restx import Resource,Namespace,fields
 from flask import request
 from models import CmmUsers,db
 import sqlalchemy as sa
+from sqlalchemy import exc
 import bcrypt
 from auth import auth
 
@@ -23,7 +24,7 @@ usr_model = ns_user.model(
         "id": fields.Integer,
         "username": fields.String,
         "password": fields.String,
-        "type": fields.String(enum=['A','L','R','V']),
+        "type": fields.String(enum=['A','L','R','V','U']),
         "date_created": fields.DateTime,
         "date_updated": fields.DateTime
     }
@@ -44,11 +45,11 @@ class UsersList(Resource):
     @ns_user.param("page","Número da página de registros","query",type=int,required=True)
     @ns_user.param("pageSize","Número de registros por página","query",type=int,required=True,default=25)
     @ns_user.param("query","Texto para busca","query")
-    #@auth.login_required
+    @auth.login_required
     def get(self):
-        pag_num  =  1 if request.args.get("page")!=None else int(request.args.get("page"))
-        pag_size = 25 if request.args.get("pageSize")!=None else int(request.args.get("pageSize"))
-        search   = "" if request.args.get("query")!=None else "{}%".format(request.args.get("query"))
+        pag_num  =  1 if request.args.get("page") is None else int(request.args.get("page"))
+        pag_size = 25 if request.args.get("pageSize") is None else int(request.args.get("pageSize"))
+        search   = "" if request.args.get("query") is None else "{}%".format(request.args.get("query"))
 
         if request.args.get("query")!=None:
             rquery = CmmUsers.query.filter(sa.and_(CmmUsers.name.like(search),CmmUsers.active==False)).paginate(page=pag_num,per_page=pag_size)
@@ -78,19 +79,21 @@ class UsersList(Resource):
     @ns_user.response(HTTPStatus.BAD_REQUEST.value,"Falha ao criar novo usuário!")
     @ns_user.param("username","Login do usuário","formData",required=True)
     @ns_user.param("password","Senha do usuário","formData",required=True)
-    @ns_user.param("type","Tipo do usuário","formData",required=True)
-    #@auth.login_required
-    def post(self)->int:
+    @ns_user.param("type","Tipo do usuário","formData",required=True,enum=['A','L','R','V','U'])
+    @auth.login_required
+    def post(self):
         try:
+            passwd = bcrypt.hashpw(request.form.get("password").encode(),bcrypt.gensalt())
             usr = CmmUsers()
-            usr.username = usr.username if request.form.get("username") is None else request.form.get("username")
-            usr.password = usr.password if request.form.get("password") is None else bcrypt.hashpw(request.form.get("password"),bcrypt.gensalt())
-            usr.type     = usr.type if request.form.get("type") is None else request.form.get("type")
+            usr.username = request.form.get("username")
+            usr.password = passwd
+            usr.type     = request.form.get("type")
+            usr.get_token()
             db.session.add(usr)
             db.session.commit()
             return usr.id
-        except:
-            return 0
+        except exc.DatabaseError as e:
+            return e.detail
 
 
 @ns_user.route("/<int:id>")
@@ -98,23 +101,23 @@ class UsersList(Resource):
 class UserApi(Resource):
     @ns_user.response(HTTPStatus.OK.value,"Obtem um registro de usuario",usr_model)
     @ns_user.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
-    #@auth.login_required
+    @auth.login_required
     def get(self,id:int):
         return CmmUsers.query.get(id).to_dict()
 
     @ns_user.response(HTTPStatus.OK.value,"Salva dados de um usuario")
     @ns_user.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
     @ns_user.param("username","Nome de login","formData",required=True)
-    @ns_user.param("password","Senha do usuário","formData",required=True)
-    @ns_user.param("type","Tipo do usuário","formData",required=True)
-    #@auth.login_required
+    @ns_user.param("password","Senha do usuário","formData")
+    @ns_user.param("type","Tipo do usuário","formData",required=True,enum=['A','L','R','V','U'])
+    @auth.login_required
     def post(self,id:int)->bool:
         try:
             usr = CmmUsers.query.get(id)
-            usr.username = usr.username if request.form.get("username")==None else request.form.get("username")
-            usr.password = usr.password if request.form.get("password")==None else request.form.get("password")
-            usr.type     = usr.type if request.form.get("type")==None else request.form.get("type")
-            usr.active   = usr.active if request.form.get("active")==None else request.form.get("active")
+            usr.username = usr.username if request.form.get("username") is None else request.form.get("username")
+            usr.password = usr.password if request.form.get("password") is None else bcrypt.hashpw(request.form.get("password").encode(),bcrypt.gensalt())
+            usr.type     = usr.type if request.form.get("type") is None else request.form.get("type")
+            usr.active   = usr.active if request.form.get("active") is None else request.form.get("active")
             db.session.commit()
             return True
         except:
@@ -122,7 +125,7 @@ class UserApi(Resource):
     
     @ns_user.response(HTTPStatus.OK.value,"Exclui os dados de um usuario")
     @ns_user.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado")
-    #@auth.login_required
+    @auth.login_required
     def delete(self,id:int)->bool:
         try:
             usr = CmmUsers.query.get(id)
@@ -142,13 +145,15 @@ class UserAuth(Resource):
         if usr:
             #verifica a senha criptografada anteriormente
             if bcrypt.checkpw(password,usr.password):
-                return {
+                obj_retorno = {
                     "token": usr.get_token(),
                     "user": {
                         "type": usr.type,
                         "name": usr.name
                     }
                 }
+                db.session.commit()
+                return obj_retorno
             else:
                 return 0 #senha invalida
         return 0 #usuario invalido
