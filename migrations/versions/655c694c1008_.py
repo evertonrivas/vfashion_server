30559880c96e926aeb1176b7f9278f5b8d1e0e38
evertonrivas <evertonrivas@gmail.com>
2023-02-23 21:58:51 -0300

    Migration

---
+++ migrations/versions/655c694c1008_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 655c694c1008
+Revises: 
+Create Date: 2023-02-23 21:58:13.318284
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '655c694c1008'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
