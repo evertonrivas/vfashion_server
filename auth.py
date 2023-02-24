39eef2af8133d0424597763686c4b34387dd737c
evertonrivas <evertonrivas@gmail.com>
2023-02-23 21:55:47 -0300

    Implementado verify_token

---
+++ auth.py
@@ -0,0 +1,19 @@
+from flask import request
+from flask_httpauth import HTTPTokenAuth
+from models import CmmUsers,db
+import sqlalchemy as sa
+import jwt
+
+auth = HTTPTokenAuth(scheme="Bearer")
+
+@auth.verify_token
+def verify_token(token):
+    try:
+        user = CmmUsers.check_token(token)
+        if user!=None:
+            data = jwt.decode(token,"VENDA_FASHION",algorithms=['HS256'])
+            if 'username' in data:
+                return True
+    except:
+        return False
+
