7c655be83aa41e1126f3168c770e5413afe5ae8c
evertonrivas <evertonrivas@gmail.com>
2023-03-13 16:15:32 -0300

    Alterado instagram para nullable

--- models.py
+++ models.py
@@ -126,7 +126,7 @@ class CmmProductsGridDistribution(db.Model,SerializerMixin):
 class CmmLegalEntities(db.Model,SerializerMixin):
     id           = sa.Column(sa.Integer,primary_key=True,nullable=False,autoincrement=True)
     name         = sa.Column(sa.String(255),nullable=False)
-    instagram    = sa.Column(sa.String(100),nullable=False)
+    instagram    = sa.Column(sa.String(100),nullable=True)
     taxvat       = sa.Column(sa.String(30),nullable=False,comment="CPF ou CNPJ no Brasil")
     state_region = sa.Column(sa.CHAR(2),nullable=False)
     city         = sa.Column(sa.String(100),nullable=False)
