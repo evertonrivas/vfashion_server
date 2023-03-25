6f61c4c5f7d06f475692cd2e3a8792e2248329cf
evertonrivas <evertonrivas@gmail.com>
2023-03-24 22:33:16 -0300

    Importação do modelo brand

--- b2b/api.py
+++ b2b/api.py
@@ -5,6 +5,7 @@ from b2b.orders import ns_order,ns_porder
 from b2b.payment_condition import ns_payment
 from b2b.customer_group import ns_group_customer
 from b2b.collection import ns_collection
+from b2b.brand import ns_brand
 
 """ Módulo Business to Business (Gestão de Vendas entre empresas)
     Módulo para realizar pedidos que realiza:
@@ -29,7 +30,7 @@ Keyword arguments: vendas, b2c, produtos, cliente, pedidos, condições de pagam
 """
 
 
-nss = [ns_price,ns_order,ns_payment,ns_porder,ns_group_customer,ns_collection]
+nss = [ns_price,ns_order,ns_payment,ns_porder,ns_group_customer,ns_collection,ns_brand]
 
 blueprint = Blueprint("b2b",__name__,url_prefix="/b2b/api/v1/")
 
