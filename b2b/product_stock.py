f65cd1b5acc3b88afa74e08d9b5a6626ae72e502
evertonrivas <evertonrivas@gmail.com>
2023-04-01 16:50:07 -0300

    Correção de nome e inclusão de fucionalidade

--- b2b/product_stock.py
+++ b2b/product_stock.py
@@ -1,8 +1,8 @@
 from http import HTTPStatus
 from flask_restx import Resource,Namespace,fields
 from flask import request
-from models import B2bProductStock,db
-from sqlalchemy import exc,or_,desc,asc
+from models import B2bProductStock, CmmTranslateColors, CmmTranslateSizes, db
+from sqlalchemy import Select, and_, exc,or_,desc,asc
 from auth import auth
 
 ns_stock = Namespace("product-stock",description="Operações para manipular dados de estoques de produtos")
@@ -36,7 +36,7 @@ stock_return = ns_stock.model(
 
 
 @ns_stock.route("/")
-class PaymentConditionsList(Resource):
+class ProductStockList(Resource):
     @ns_stock.response(HTTPStatus.OK.value,"Obtem a lista de estoques de produtos do B2B",stock_return)
     @ns_stock.response(HTTPStatus.BAD_REQUEST.value,"Falha ao listar registros!")
     @ns_stock.param("page","Número da página de registros","query",type=int,required=True)
@@ -106,7 +106,7 @@ class PaymentConditionsList(Resource):
     @ns_stock.param("name","Nome da condição de pagamento","formData",required=True)
     @ns_stock.param("received_days","Dias para recebimento","formData",type=int,required=True)
     @ns_stock.param("installments","Número de parcelas","formData",type=int,required=True)
-    #@auth.login_required
+    @auth.login_required
     def post(self)->int:
         try:
             stock = B2bProductStock()
@@ -129,9 +129,10 @@ class PaymentConditionsList(Resource):
 @ns_stock.param("id","Id do produto")
 @ns_stock.param("color","Cor do produto")
 @ns_stock.param("size","Tamanho do produto")
-class PaymentConditionApi(Resource):
+class ProductStockApi(Resource):
     @ns_stock.response(HTTPStatus.OK.value,"Obtem um registro do estoque de um produto do B2B",stock_model)
     @ns_stock.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado!")
+    @auth.login_required
     def get(self,id:int,color:str,size:str):
         try:
             return B2bProductStock.query.get([id,color,size]).to_dict()
@@ -163,7 +164,7 @@ class PaymentConditionApi(Resource):
     
     @ns_stock.response(HTTPStatus.OK.value,"Exclui os dados de uma condição de pagamento")
     @ns_stock.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado!")
-    #@auth.login_required
+    @auth.login_required
     def delete(self,id:int,color:str,size:str)->bool:
         try:
             payCond = B2bProductStock.query.get([id,color,size])
@@ -176,3 +177,50 @@ class PaymentConditionApi(Resource):
                 "error_details": e._message(),
                 "error_sql": e._sql_message()
             }
+
+class ProductStockLoad(Resource):
+    @ns_stock.response(HTTPStatus.OK.value,"Obtem a lista de estoques de um determinado produto do B2B")
+    @ns_stock.response(HTTPStatus.BAD_REQUEST.value,"Falha ao listar registros!")
+    @auth.login_required
+    def get(self,id_product:int):    
+        cquery = Select(B2bProductStock.color,CmmTranslateColors.hexcode,CmmTranslateColors.name)\
+            .distinct(B2bProductStock.color)\
+            .join(CmmTranslateColors,CmmTranslateColors.color==B2bProductStock.color)\
+            .filter(B2bProductStock.id_product==id_product)\
+            .order_by(asc(B2bProductStock.color))
+
+        cquery = db.session.execute(cquery)
+        
+        return [{
+            "color_name": m.name,
+            "color_hexa": m.hexcode,
+            "color_code": m.color,
+            "sizes": self.get_sizes(id_product,m.color)
+        }for m in cquery]
+    
+    def get_sizes(self,id_product:int,color:str):
+        subquery = Select(B2bProductStock.quantity,B2bProductStock.limited,B2bProductStock.size.label("size_code"))\
+            .where(and_(B2bProductStock.id_product==id_product,B2bProductStock.color==color))\
+            .cte()
+        query = Select(CmmTranslateSizes.size.label("size_code"),CmmTranslateSizes.size_name,subquery.c.quantity,subquery.c.limited)\
+            .outerjoin(subquery,subquery.c.size_code==CmmTranslateSizes.size)
+        
+        #print(query)
+
+        query = db.session.execute(query)
+
+        return [{
+                "size_code": s.size_code,
+                "size_name": s.size_name,
+                "size_value": self.formatQuantity(s.quantity,s.limited)
+            }for s in query]
+    
+    def formatQuantity(self,quantity:int,limited:bool):
+        if quantity is None and limited==False:
+            return 99999
+        if quantity is None  and limited is None:
+            return None
+        return quantity
+
+
+ns_stock.add_resource(ProductStockLoad,"/load-by-product/<int:id_product>")
