47341b3673ca0eb94fa86506f82aa8e5a5df02c6
evertonrivas <evertonrivas@gmail.com>
2023-04-26 17:46:35 -0300

    Busca clientes dos reps

--- integrations/virtualage.py
+++ integrations/virtualage.py
@@ -154,7 +154,7 @@ class VirtualAge(ERP):
                         "filter":{
                             "representativeCodeList": ConfigVirtualAge.ACTIVE_REPS.value
                         },
-                        "expand": "addresses,emails,phones",
+                        "expand": "addresses,emails,phones,customers",
                         "page" : act_page
                     }
                 else:
@@ -192,27 +192,15 @@ class VirtualAge(ERP):
 
                                 #importa os emails do cadastro
                                 for em in it.emails:
-                                    conn.execute(Insert(CmmLegalEntityContact).values(
-                                        id_legal_entity=res.inserted_primary_key[0],
-                                        contact_type = 'E',
-                                        is_whatsapp = False,
-                                        value = str(em.email).replace(" ",""),
-                                        is_default = bool(em.isDefault),
-                                        name = em.typeName
-                                    ))
-                                    conn.commit()
+                                    self.__save_contact(conn,em,"E",res.inserted_primary_key[0])
 
                                 #importa os telefones do cadastro
                                 for ph in it.phones:
-                                    conn.execute(Insert(CmmLegalEntityContact).values(
-                                        id_legal_entity=res.inserted_primary_key[0],
-                                        contact_type = 'P',
-                                        is_whatsapp = False,
-                                        value = str(ph.number).replace(" ","").replace("(","").replace(")","").replace("-",""),
-                                        is_default = ph.isDefault,
-                                        name = ph.typeName
-                                    ))
-                                    conn.commit()
+                                    self.__save_contact(conn,ph,"P",res.inserted_primary_key[0])
+
+                                if it.customers!=None:
+                                    for cs in it.customers:
+                                        self.get_customer(False,cs.cpfCnpj)
                             else:
                                 #aqui irah atualizar as informacoes de cadastro
                                 conn.execute(Update(CmmLegalEntities).values(
@@ -228,29 +216,17 @@ class VirtualAge(ERP):
                                 for em in it.emails:
                                     exist = conn.execute(Select(CmmLegalEntityContact).where(CmmLegalEntityContact.value==em.email)).first()
                                     if exist==None:
-                                        conn.execute(Insert(CmmLegalEntityContact).values(
-                                            id_legal_entity=exist.id,
-                                            contact_type = 'E',
-                                            is_whatsapp = False,
-                                            value = str(em.email).replace(" ",""),
-                                            is_default = bool(em.isDefault),
-                                            name = em.typeName
-                                        ))
-                                        conn.commit()
+                                        self.__save_contact(conn,em,"E",exist.id)
                                 
                                 for ph in it.phones:
                                     number = str(ph.number).replace(" ","").replace("(","").replace(")","").replace("-","")
                                     exist = conn.execute(Select(CmmLegalEntityContact).where(CmmLegalEntityContact.value==number))
                                     if exist==None:
-                                        conn.execute(Insert(CmmLegalEntityContact).values(
-                                            id_legal_entity=exist.id,
-                                            contact_type = 'P',
-                                            is_whatsapp = False,
-                                            value = str(ph.number).replace(" ","").replace("(","").replace(")","").replace("-",""),
-                                            is_default = bool(ph.isDefault),
-                                            name = ph.typeName
-                                        ))
-                                        conn.commit()
+                                        self.__save_contact(conn,ph,"P",exist.id)
+                                
+                                if it.customers!=None:
+                                    for cs in it.customers:
+                                        self.get_customer(False,cs.cpfCnpj)
                 else:
                     # print(req.status_code)
                     # print(req.text)
@@ -261,15 +237,114 @@ class VirtualAge(ERP):
             print(e)
             return False
     
-    def get_customer(self,_taxvat:str):
-        req = self.nav.post(ConfigVirtualAge.URL.value+'',
-                              headers=self._get_header(),data={
-                                
-                              })
-        if req.status_code==200:
-            return self._as_object(req)
-        return False
+    def get_customer(self,all:bool=True,taxvat:str=""):
+        try:
+            act_page = 1
+            has_next = True
+            while has_next:
+
+                if all==False:
+                    filter = {
+                        "filter":{
+                            "isCustomer": True,
+                            "isSupplier": False,
+                            "isRepresentative": False,
+                            "isPurchasingGuide": False,
+                            "isShippingCompany": False,
+                            "cnpjList": [taxvat]
+                        },
+                        "expand": "phones,emails,addresses",
+                        "page": act_page,
+                        "pageSize": 500
+                    }
+                else:
+                    filter = {
+                        "filter":{
+                            "isCustomer": True,
+                            "isSupplier": False,
+                            "isRepresentative": False,
+                            "isPurchasingGuide": False,
+                            "isShippingCompany": False
+                        },
+                        "expand": "phones,emails,addresses",
+                        "page": act_page,
+                        "pageSize": 500
+                    }
+
+                req = self.nav.post(ConfigVirtualAge.URL.value+'/api/totvsmoda/person/v2/legal-entities/search',headers=self._get_header(),data=json.dumps(filter))
+                if req.status_code==200:
+                    data = self._as_object(req)
+                    for it in data.items:
+                        with self.dbconn.connect() as conn:
+                            clear_cnpj = str(it.cnpj).replace(" ","").replace("/","").replace("-","").replace(".","")
+                            exist = conn.execute(Select(CmmLegalEntities).where(CmmLegalEntities.taxvat==clear_cnpj)).first()
+                            if exist==None:
+                                resp = conn.execute(Insert(CmmLegalEntities).values(
+                                    origin_id = it.code,
+                                    name = it.name,
+                                    fantasy_name = it.fantasyName,
+                                    taxvat = clear_cnpj,
+                                    state_region = it.addresses[0].stateAbbreviation,
+                                    city = it.addresses[0].cityName,
+                                    postal_code = it.addresses[0].cep,
+                                    neighborhood = it.addresses[0].neighborhood,
+                                    type="C"
+                                ))
+                                conn.commit()
+
+                                #importa os emails do cadastro
+                                for em in it.emails:
+                                    self.__save_contact(conn,em,"E",resp.inserted_primary_key[0])
+
+                                #importa os telefones do cadastro
+                                for ph in it.phones:
+                                    self.__save_contact(conn,ph,"P",resp.inserted_primary_key[0])
+                            else:
+                                resp = conn.execute(Update(CmmLegalEntities).values(
+                                    origin_id = it.code,
+                                    name = it.name,
+                                    fantasy_name = it.fantasyName,
+                                    state_region = it.addresses[0].stateAbbreviation,
+                                    city = it.addresses[0].cityName,
+                                    postal_code = it.addresses[0].cep,
+                                    neighborhood = it.addresses[0].neighborhood,
+                                    type="C"
+                                ).where(CmmLegalEntities.taxvat==clear_cnpj))
+                                conn.commit()
+
+                                for em in it.emails:
+                                        exist = conn.execute(Select(CmmLegalEntityContact).where(CmmLegalEntityContact.value==em.email)).first()
+                                        if exist==None:
+                                            self.__save_contact(conn,em,"E",exist.id)
+                                    
+                                for ph in it.phones:
+                                    number = str(ph.number).replace(" ","").replace("(","").replace(")","").replace("-","")
+                                    exist = conn.execute(Select(CmmLegalEntityContact).where(CmmLegalEntityContact.value==number))
+                                    if exist==None:
+                                        self.__save_contact(conn,ph,"P",exist.id)
+
+                else:
+                    print(req.status_code)
+                    print(req.text)
+                    has_next = False
+                
+                has_next = data.hasNext
+                act_page += 1
+        except RequestException as e:
+            print(e)
+            return False
     
+    def __save_contact(self,conn,obj,contact_type,id = None):
+        conn.execute(Insert(CmmLegalEntityContact).values(
+                id_legal_entity=id,
+                contact_type = contact_type,
+                is_whatsapp = False,
+                value = str(obj.email).replace(" ","") if contact_type=="E" else str(obj.number).replace(" ","").replace("(","").replace(")","").replace("-",""),
+                is_default = bool(obj.isDefault),
+                name = obj.typeName
+            ))
+        conn.commit()
+
     def create_order(self):
         pass
 
