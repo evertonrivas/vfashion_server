a41e223dec818168de1438fcafd27804c12dbfb1
evertonrivas <evertonrivas@gmail.com>
2023-06-21 22:15:18 -0300

    Improved Search engine and added EntityOfStage

--- cmm/legal_entities.py
+++ cmm/legal_entities.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 from flask_restx import Resource,Namespace,fields
 from flask import request
-from models import CmmLegalEntities,CmmUserEntity,db
+from models import CmmCities, CmmCountries, CmmLegalEntityContact, CmmStateRegions, CrmFunnelStageCustomer, _get_params,CmmLegalEntities,CmmUserEntity,db
 from sqlalchemy import Select,and_,exc,asc,desc,func
 from auth import auth
 from config import Config
@@ -54,55 +54,106 @@ class EntitysList(Resource):
         pag_num   =  1 if request.args.get("page") is None else int(request.args.get("page"))
         pag_size  = Config.PAGINATION_SIZE.value if request.args.get("pageSize") is None else int(request.args.get("pageSize"))
         search    = "" if request.args.get("query") is None else request.args.get("query")
-        order_by  = "id" if request.args.get("order_by") is None else request.args.get("order_by")
-        direction = asc if request.args.get("order_dir") == 'ASC' else desc
-        list_all = False if request.args.get("list_all") == 'false' else True
 
         try:
-            if search!="":
-                if search.find("is:query ")!=-1:
-                    nsearch = search.split("is:query ")[1]
-                    nsearch = "%{}%".format(nsearch)
-                    rquery = CmmLegalEntities.query.filter(and_(CmmLegalEntities.trash == False,CmmLegalEntities.name.like(search))).order_by(direction(getattr(CmmLegalEntities, order_by)))
-                else:
-                    pass
-            else:
-                rquery = CmmLegalEntities.query.filter(CmmLegalEntities.trash == False).order_by(direction(getattr(CmmLegalEntities, order_by)))
+            params = _get_params(search)
 
-            if list_all==False:
-                rquery = rquery.paginate(page=pag_num,per_page=pag_size)
+            direction = asc if hasattr(params,'order')==False else asc if params.order=='ASC' else desc
+            order_by  = 'id' if hasattr(params,'order_by')==False else params.order_by
+            search = None if hasattr(params,"search")==False else params.search
+
+            rquery = Select(
+                    CmmLegalEntities.id,
+                    CmmLegalEntities.name.label("social_name"),
+                    CmmLegalEntities.fantasy_name,
+                    CmmLegalEntities.postal_code,
+                    CmmLegalEntities.neighborhood,
+                    CmmLegalEntities.taxvat,
+                    CmmLegalEntities.type,
+                    CmmLegalEntities.date_created,
+                    CmmLegalEntities.date_updated,
+                    CmmCities.id.label("city_id"),
+                    CmmCities.name.label("city_name"),
+                    CmmCities.brazil_ibge_code,
+                    CmmStateRegions.id.label("state_id"),
+                    CmmStateRegions.name.label("state_name"),
+                    CmmStateRegions.acronym,
+                    CmmCountries.id.label("country_id"),
+                    CmmCountries.name.label("country_name"))\
+                .join(CmmCities,CmmCities.id==CmmLegalEntities.id_city)\
+                .join(CmmStateRegions,CmmStateRegions.id==CmmCities.id_state_region)\
+                .join(CmmCountries,CmmCountries.id==CmmStateRegions.id_country)\
+                .order_by(direction(getattr(CmmLegalEntities,order_by)))
+            
+            if search!=None:
+                rquery = rquery.where(
+                    CmmCountries.name.like(search) | 
+                    CmmStateRegions.name.like(search) |
+                    CmmCities.name.like(search) |
+                    CmmLegalEntities.name.like(search) |
+                    CmmLegalEntities.fantasy_name.like(search))
+
+            if hasattr(params,'list_all')==False:
+                pag = db.paginate(rquery,page=pag_num,per_page=pag_size)
+
                 return {
                     "pagination":{
-                        "registers": rquery.total,
+                        "registers": pag.total,
                         "page": pag_num,
                         "per_page": pag_size,
-                        "pages": rquery.pages,
-                        "has_next": rquery.has_next
+                        "pages": pag.pages,
+                        "has_next": pag.has_next
                     },
                     "data":[{
                         "id": m.id,
-                        "name": m.name,
+                        "name": m.social_name,
+                        "fantasy_name": m.fantasy_name,
                         "taxvat": m.taxvat,
+                        "city": {
+                            "id": m.city_id,
+                            "name": m.city_name,
+                            "stage_region": {
+                                "id": m.state_id,
+                                "name": m.state_name,
+                                "acronym": m.acronym,
+                                "country":{
+                                    "id": m.country_id,
+                                    "name": m.country_name
+                                }
+                            }
+                        },
                         "postal_code": m.postal_code,
                         "neighborhood": m.neighborhood,
                         "type": m.type,
                         "date_created": m.date_created.strftime("%Y-%m-%d %H:%M:%S"),
                         "date_updated": m.date_updated.strftime("%Y-%m-%d %H:%M:%S") if m.date_updated!=None else None
-                    } for m in rquery.items]
+                    } for m in db.session.execute(rquery)]
                 }
             else:
                 return [{
                         "id": m.id,
-                        "name": m.name,
+                        "name": m.social_name,
+                        "fantasy_name": m.fantasy_name,
                         "taxvat": m.taxvat,
-                        "state_region":m.state_region,
-                        "city": m.city,
+                        "city": {
+                            "id": m.city_id,
+                            "name": m.city_name,
+                            "stage_region": {
+                                "id": m.state_id,
+                                "name": m.state_name,
+                                "acronym": m.acronym,
+                                "country":{
+                                    "id": m.coutry_id,
+                                    "name": m.coutry_name
+                                }
+                            }
+                        },
                         "postal_code": m.postal_code,
                         "neighborhood": m.neighborhood,
                         "type": m.type,
                         "date_created": m.date_created.strftime("%Y-%m-%d %H:%M:%S"),
                         "date_updated": m.date_updated.strftime("%Y-%m-%d %H:%M:%S") if m.date_updated!=None else None
-                    } for m in rquery.all()]
+                    } for m in db.session.execute(rquery).all()]
         except exc.SQLAlchemyError as e:
             return {
                 "error_code": e.code,
@@ -225,3 +276,124 @@ class EntityCount(Resource):
             }
 
 ns_legal.add_resource(EntityCount,'/count')
+
+class EntityOfStage(Resource):
+    @ns_legal.response(HTTPStatus.OK.value,"Retorna o total de Entidades por tipo")
+    @ns_legal.response(HTTPStatus.BAD_REQUEST.value,"Registro não encontrado!")
+    @ns_legal.param("page","Número da página de registros","query",type=int,required=True)
+    @ns_legal.param("pageSize","Número de registros por página","query",type=int,required=True,default=25)
+    @auth.login_required
+    def get(self,id:int):
+        pag_num   =  1 if request.args.get("page") is None else int(request.args.get("page"))
+        pag_size  = Config.PAGINATION_SIZE.value if request.args.get("pageSize") is None else int(request.args.get("pageSize"))
+
+        try:
+            params = _get_params(request.args.get("query"))
+
+            direction = asc if hasattr(params,'order')==False else asc if params.order=='ASC' else desc
+            order_by  = 'id' if hasattr(params,'order_by')==False else params.order_by
+            search = None if hasattr(params,"search")==False else params.search
+
+            print(params)
+
+            rquery = Select(
+                CmmLegalEntities.id,
+                CmmLegalEntities.name.label("social_name"),
+                CmmLegalEntities.fantasy_name,
+                CmmLegalEntities.postal_code,
+                CmmLegalEntities.neighborhood,
+                CmmLegalEntities.taxvat,
+                CmmLegalEntities.type,
+                CmmLegalEntities.date_created,
+                CmmLegalEntities.date_updated,
+                CmmCities.id.label("city_id"),
+                CmmCities.name.label("city_name"),
+                CmmCities.brazil_ibge_code,
+                CmmStateRegions.id.label("state_id"),
+                CmmStateRegions.name.label("state_name"),
+                CmmStateRegions.acronym,
+                CmmCountries.id.label("country_id"),
+                CmmCountries.name.label("country_name"))\
+            .join(CmmCities,CmmCities.id==CmmLegalEntities.id_city)\
+            .join(CmmStateRegions,CmmStateRegions.id==CmmCities.id_state_region)\
+            .join(CmmCountries,CmmCountries.id==CmmStateRegions.id_country)\
+            .join(CrmFunnelStageCustomer,CrmFunnelStageCustomer.id_customer==CmmLegalEntities.id)\
+            .where(CrmFunnelStageCustomer.id_funnel_stage==id)\
+            .order_by(direction(getattr(CmmLegalEntities,order_by)))
+
+            if search!=None:
+                rquery = rquery.where(
+                    CmmCountries.name.like(search) | 
+                    CmmStateRegions.name.like(search) |
+                    CmmCities.name.like(search) |
+                    CmmLegalEntities.name.like(search) |
+                    CmmLegalEntities.fantasy_name.like(search) | 
+                    CmmLegalEntities.neighborhood.like(search)
+                )
+                
+            pag = db.paginate(rquery,page=pag_num,per_page=pag_size)
+
+            rquery = rquery.limit(pag_size).offset((pag_num - 1) * pag_size)
+
+            return {
+                    "pagination":{
+                        "registers": pag.total,
+                        "page": pag_num,
+                        "per_page": pag_size,
+                        "pages": pag.pages,
+                        "has_next": pag.has_next,
+                    },
+                    "data":[{
+                        "id": m.id,
+                        "name": m.social_name,
+                        "fantasy_name": m.fantasy_name,
+                        "taxvat": m.taxvat,
+                        "city": {
+                            "id": m.city_id,
+                            "name": m.city_name,
+                            "brazil_ibge_code":m.brazil_ibge_code,
+                            "state_region": {
+                                "id": m.state_id,
+                                "name": m.state_name,
+                                "acronym": m.acronym,
+                                "country":{
+                                    "id": m.country_id,
+                                    "name": m.country_name
+                                }
+                            }
+                        },
+                        "contacts": self.__get_contacts(m.id),
+                        "postal_code": m.postal_code,
+                        "neighborhood": m.neighborhood,
+                        "type": m.type,
+                        "date_created": m.date_created.strftime("%Y-%m-%d %H:%M:%S"),
+                        "date_updated": m.date_updated.strftime("%Y-%m-%d %H:%M:%S") if m.date_updated!=None else None
+                    } for m in db.session.execute(rquery)]
+                }
+        except exc.SQLAlchemyError as e:
+            return {
+                "error_code": e.code,
+                "error_details": e._message(),
+                "error_sql": e._sql_message()
+            }
+        
+    def __get_contacts(self,id_customer:int):
+        stmt = Select(CmmLegalEntityContact.id,
+                      CmmLegalEntityContact.name,
+                      CmmLegalEntityContact.contact_type,
+                      CmmLegalEntityContact.value,
+                      CmmLegalEntityContact.is_whatsapp,
+                      CmmLegalEntityContact.is_default)\
+            .join(CmmLegalEntities,CmmLegalEntities.id==CmmLegalEntityContact.id_legal_entity)\
+            .where(CmmLegalEntities.id==id_customer)
+        
+        return [{
+            "id": c.id,
+            "name": c.name,
+            "contact_type": c.contact_type,
+            "value": c.value,
+            "is_whatsapp": c.is_whatsapp, #E = E-mail, P = Phone
+            "is_default": c.is_default
+        } for c in db.session.execute(stmt)]
+
+ns_legal.add_resource(EntityOfStage,'/by-crm-stage/<int:id>')
