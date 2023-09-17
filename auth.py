0192df339382aba66133dace44c109ab2b70661a
evertonrivas <evertonrivas@gmail.com>
2023-09-17 12:13:59 -0300

    Token improvement

--- auth.py
+++ auth.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_httpauth import HTTPTokenAuth
 from models import CmmUsers,db
 import jwt
+from config import Config
 
 auth = HTTPTokenAuth(scheme="Bearer")
 
@@ -13,7 +14,7 @@ def verify_token(token):
         if user!=None:
             try:
                 complete_key = datetime.now().year+datetime.now().month+datetime.now().day
-                data = jwt.decode(token,"VENDA_FASHION_"+str(complete_key),algorithms=['HS256'])
+                data = jwt.decode(token,Config.TOKEN_KEY.value+str(complete_key),algorithms=['HS256'])
             except Exception as e:
                 print(e)
             if 'username' in data:
