134f34edfc49bd4c26d773712a7a4afc706f58dc
evertonrivas <evertonrivas@gmail.com>
2023-09-18 18:55:05 -0300

    improved upload

--- cmm/upload.py
+++ cmm/upload.py
@@ -1,37 +1,91 @@
 from http import HTTPStatus
 from flask_restx import Resource,Namespace,fields
 from flask import request
-from sqlalchemy import Select, desc, exc, asc
-import werkzeug
+from sqlalchemy import Select, desc, exc, asc,Delete
+from werkzeug import exceptions
 from auth import auth
-from config import Config
+from config import Config, CustomerAction
+from datetime import datetime
 import filetype
+from werkzeug.datastructures import ImmutableMultiDict
+from models import db,_save_log,CmmLegalEntities,CmmLegalEntityFile
+import os
 
 ns_upload = Namespace("upload",description="Operações para manipular upload de dados")
 
+@ns_upload.route("/<int:id>")
 class UploadList(Resource):
     @ns_upload.response(HTTPStatus.OK.value,"Obtem a listagem de cidades")
     @ns_upload.response(HTTPStatus.BAD_REQUEST.value,"Falha ao listar registros!")
     @ns_upload.param("file","Arquivo a ser enviado para o servidor","formData")
-    def post(self):
+    @auth.login_required
+    def post(self,id:int):
         try:
-            file = request.files['file']
-            ftype = filetype.guess_mime(file.name)
-            fpath = Config.APP_PATH.value
-            if ftype=='application/pdf':
-                fpath += 'assets/pdf/'
-            elif ftype=='application/msword' or ftype=='application/vnd.ms-excel' or ftype=='application/vnd.ms-powerpoint':
-                fpath += 'assets/docs/'
-            elif ftype=='text/plain' or ftype=='text/html' or ftype=='text/richtext':
-                fpath += 'assets/docs/'
-            elif ftype=='image/png' or ftype=='image/jpeg' or ftype=='image/bmp' or ftype=='image/gif' or ftype=='image/svg+xml' or ftype=='image/tiff':
-                fpath += 'assets/images/'
-            else:
-                fpath += 'assets/unclassified/'
-            
-            file.save(fpath+'assets/')
+            newFileName = ''
+            #obtem os arquivos para upload
+            files = []
+            fileCount = 1
+            data = ImmutableMultiDict(request.files)
+            for file in data.getlist('files[]'):
+                parts = file.filename.split(".")
+                ext = parts[len(parts)-1]
+                fpath = Config.APP_PATH.value+'assets/'
+                if ext=='pdf':
+                    ffolder = 'pdf/'
+                elif ext=='doc' or ext=='docx' or ext=='xls' or ext=='xlsx' or ext=='ppt' or ext=='pptx':
+                    ffolder = 'docs/'
+                elif ext=='txt' or ext=='html' or ext=='rtl' or ext=='csv':
+                    ffolder = 'docs/'
+                elif ext=='png' or ext=='jpeg' or ext=='jpg' or ext=='bmp' or ext=='gif' or ext=='svg' or ext=='tiff':
+                    ffolder = 'images/'
+                elif ext=='cer' or ext=='p7b' or ext=='pfx' or ext=='p12':
+                    ffolder = 'certs/'
+                else:
+                    ffolder = 'unclassified/'
+
+
+                #busca as informacoes de cliente para salvar o arquivo
+                #tambem atribui um novo nome ao arquivo
+                entity = db.session.execute(Select(CmmLegalEntities.taxvat).where(CmmLegalEntities.id==id)).first()
+                if entity is not None:
+                    #salva o arquivo com outro nome
+                    newFileName = entity.taxvat+"_"+str(fileCount)+"_"+datetime.now().strftime("%Y%m%d-%H%M%S")+"."+ext
+                    files.append(newFileName)
+                    file.save(fpath+ffolder+newFileName)
+
+                    #salva os dados do arquivo no cadastro do cliente
+                    file = CmmLegalEntityFile()
+                    file.id_legal_entity = id
+                    file.content_type = 'Arquivo '+ext if filetype.guess_mime(fpath+ffolder+newFileName) is None else filetype.guess_mime(fpath+ffolder+newFileName)
+                    file.folder = ffolder
+                    file.name = newFileName
+                    db.session.add(file)
+                    db.session.commit()
+                fileCount += 1
+
+            combinedFiles = ','.join(files)
+            _save_log(id,CustomerAction.FA,"Adicionados os arquivos "+combinedFiles)
+
             return True
-        except werkzeug.exceptions.HTTPException as e:
-            print(e)
+        except exceptions.HTTPException as e:
+            print("Exception")
+            print(e.get_headers())
             return False
-        
+    
+    def delete(self,id:int):
+        try:
+            file = db.session.execute(Select(CmmLegalEntityFile.name,
+                                             CmmLegalEntityFile.folder,
+                                             CmmLegalEntityFile.id_legal_entity,
+                                             CmmLegalEntityFile.content_type,
+                                             CmmLegalEntityFile.date_created,
+                                             CmmLegalEntityFile.date_updated).where(CmmLegalEntityFile.id==id)).first()
+            if file is not None:
+                if os.path.exists(Config.APP_PATH.value+'assets/'+str(file.folder)+str(file.name)):
+                    os.remove(Config.APP_PATH.value+'assets/'+str(file.folder)+str(file.name))
+                    db.session.execute(Delete(CmmLegalEntityFile).where(CmmLegalEntityFile.id==id))
+                    db.session.commit()
+                    _save_log(file.id_legal_entity,CustomerAction.FD,'Removido o arquivo '+file.name)
+                return True
+        except exceptions.HTTPException as e:
+            return False
