4616d5d3b2903466bd118bd8df556788e2cd7a41
evertonrivas <evertonrivas@gmail.com>
2023-10-06 23:19:40 -0300

    _send_email improved

--- common.py
+++ common.py
@@ -1,13 +1,9 @@
-from config import Config,MailTemplates
+from config import ConfigEmail,EmailLib,Config,MailTemplates
 import jinja2
-import filetype
 import pdfkit
-import base64
 import os
 import html.entities
-import pathlib
 from datetime import date,datetime
-from sqlalchemy import Select,func,and_,or_
 import requests
 
 def _gen_pdf():
@@ -17,13 +13,13 @@ def _gen_pdf():
 
         tplLoader  = jinja2.FileSystemLoader(searchpath=Config.APP_PATH.value+'assets/layout/')
         tplEnv     = jinja2.Environment(loader=tplLoader)
-        layoutFile = "layout.html"
+        layoutFile = "pdf_layout.html"
         bodyReport = tplEnv.get_template(layoutFile)
 
-        headerFile   = "header.html"
+        headerFile   = "pdf_header.html"
         headerReport = tplEnv.get_template(headerFile)
 
-        footerFile   = "footer.html"
+        footerFile   = "pdf_footer.html"
         footerReport = tplEnv.get_template(footerFile)
 
 
@@ -36,10 +32,10 @@ def _gen_pdf():
         )
         
         #apaga se o arquivo existir
-        if os.path.exists(Config.APP_PATH.value+'assets/layout/header_tmp.html')==True:
-            os.remove(Config.APP_PATH.value+'assets/layout/header_tmp.html')
+        if os.path.exists(Config.APP_PATH.value+'assets/layout/pdf_header_tmp.html')==True:
+            os.remove(Config.APP_PATH.value+'assets/layout/pdf_header_tmp.html')
 
-        with open(Config.APP_PATH.value+'assets/layout/header_tmp.html',"w") as file_header:
+        with open(Config.APP_PATH.value+'assets/layout/pdf_header_tmp.html',"w") as file_header:
             file_header.write(header_txt)
             file_header.close()
         #-----------------------------------------------------#
@@ -55,10 +51,10 @@ def _gen_pdf():
         )
 
         #apaga se o arquivo existir
-        if os.path.exists(Config.APP_PATH.value+'assets/layout/footer_tmp.html')==True:
-            os.remove(Config.APP_PATH.value+'assets/layout/footer_tmp.html')
+        if os.path.exists(Config.APP_PATH.value+'assets/layout/pdf_footer_tmp.html')==True:
+            os.remove(Config.APP_PATH.value+'assets/layout/footer_pdf_tmp.html')
 
-        with open(Config.APP_PATH.value+'assets/layout/footer_tmp.html',"w") as file_footer:
+        with open(Config.APP_PATH.value+'assets/layout/pdf_footer_tmp.html',"w") as file_footer:
             file_footer.write(footer_txt)
             file_footer.close()
         #-----------------------------------------------------#
@@ -85,5 +81,102 @@ def _gen_pdf():
         print(e)
         return False
     
-def _send_email(p_to:str,p_subject:str,p_content:str,p_tpl:MailTemplates,p_attach:str=None)->bool:
-    return False
+def _send_email(p_to:[str],p_subject:str,p_content:str,p_tpl:MailTemplates,p_attach:[str]=None)->bool:
+
+    try:
+        tplLoader  = jinja2.FileSystemLoader(searchpath=Config.APP_PATH.value+'assets/layout/')
+        tplEnv     = jinja2.Environment(loader=tplLoader)
+        layoutFile = MailTemplates.DEFAULT.value
+        mailTemplate = tplEnv.get_template(layoutFile)
+        mail_template = mailTemplate.render(
+            variavel_existente_no_layout=''
+        )
+
+        if(ConfigEmail.LIB==EmailLib.GMAIL):
+            import smtplib,ssl
+            from email import encoders
+            from email.mime.base import MIMEBase
+            from email.mime.text import MIMEText
+            from email.mime.multipart import MIMEMultipart
+
+            message = MIMEMultipart("alternative")
+            message["Subject"] = p_subject
+            message["From"]    = ConfigEmail.GMAIL_USER
+            message["To"]      = ",".join(p_to)
+            message.attach(MIMEText(mail_template,"html"))
+
+            if len(p_attach)>0:
+                for att in p_attach:
+                    with open(Config.APP_PATH+'assets/tmp/'+att['name']) as f:
+                        part = MIMEBase("application","octet-stream")
+                        part.set_payload(f.read())
+                
+                encoders.encode_base64(part)
+                part.add_header(
+                    "Content-Disposition",
+                    f"attachment; filename={att['name']}"
+                )
+                message.attach(part)
+
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL("smtp.gmail.com",587,context=context) as server:
+                server.login(ConfigEmail.GMAIL_USER,ConfigEmail.GMAIL_PASS)
+                server.sendmail(ConfigEmail.GMAIL_USER,p_to,message.as_string())
+            return True
+        elif(ConfigEmail.LIB==EmailLib.SMTP):
+            import smtplib,ssl
+            from email.mime.text import MIMEText
+            from email.mime.multipart import MIMEMultipart
+
+            pass
+        elif(ConfigEmail.LIB==EmailLib.SENDGRID):
+            #abre o arquivo
+            if len(p_attach)>0:
+                json_content = {
+                    "personalizations":[{
+                        "to":[{
+                            "email": one_to
+                        }for one_to in p_to.split(",")],
+                        "subject": p_subject
+                    }],
+                    "from":{
+                        "email":ConfigEmail.SEND_GRID_TO,
+                        "name": ConfigEmail.SEND_GRID_TO_NAME
+                    },
+                    "content":[{
+                        "type": "text/html",
+                        "value": mail_template
+                    }],
+                    "attachments":[{
+                        "content": att['content'],
+                        "type": att['content_type'],
+                        "fileName": att['name']
+                    }for att in p_attach]
+                }
+            else:
+                json_content = {
+                    "personalizations":[{
+                        "to":[{
+                            "email": one_to
+                        }for one_to in p_to.split(",")],
+                        "subject": p_subject
+                    }],
+                    "from":{
+                        "email": ConfigEmail.SEND_GRID_TO,
+                        "name": ConfigEmail.SEND_GRID_TO_NAME
+                    },
+                    "content":[{
+                        "type": "text/html",
+                        "value": mail_template
+                    }]
+                }
+            
+            resp = requests.post("https://api.sendgrid.com/v3/mail/send",json=json_content,headers={
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '+ConfigEmail.SEND_GRID_TOKEN.value
+            })
+            if resp.ok:
+                return True
+            return False
+    except Exception as e:
+        return False
