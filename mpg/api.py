bb34ea78714d15c47c0d7510ba919c2e686e61c6
evertonrivas <evertonrivas@gmail.com>
2023-10-11 15:48:09 -0300

    Added Marketing Plan Generator

---
+++ mpg/api.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+from flask_restx import Api
+# from pos.consumer import api as ns_consumer
+# from pos.consumer import apis as ns_consumer_group
+
+
+# nss = [ns_consumer,ns_consumer_group]
+
+blueprint = Blueprint("mpg",__name__,url_prefix="/mpg/api/")
+
+api = Api(blueprint,
+    version="1.0",
+    title="API Fast2Bee",
+    description="Uma API REST para o sistema CLM - Módulo MPG",
+    contact_email="evertonrivas@gmail.com",
+    contact="Fast2Bee",
+    contact_url="http://www.fast2bee.com")
+
+# for ns in nss:
+#     api.add_namespace(ns)
