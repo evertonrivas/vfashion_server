4aa2f8cfc2a977669f6a124c6de61465c7d962cb
evertonrivas <evertonrivas@gmail.com>
2024-07-22 14:59:07 -0300

    new upload for import

--- cmm/upload.py
+++ cmm/upload.py
@@ -113,7 +113,7 @@ class UploadTmp(Resource):
 ns_upload.add_resource(UploadTmp,'/temp')
 
 @ns_upload.route("/<int:id>/<int:idprod>/<int:idcolor>/<int:idsize>")
-class UploadReturn(Resource):
+class UploadDevolution(Resource):
     @ns_upload.response(HTTPStatus.OK.value,"Realiza envio de arquivo(s) para o servidor na pasta temporaria")
     @ns_upload.response(HTTPStatus.BAD_REQUEST.value,"Falha ao enviar arquivo(s)!")
     @auth.login_required
@@ -136,4 +136,28 @@ class UploadReturn(Resource):
         except exceptions.HTTPException as e:
             print(e)
             return False
-ns_upload.add_resource(UploadReturn,'/return/<int:id>/<int:idprod>/<int:idcolor>/<int:idsize>')
+ns_upload.add_resource(UploadDevolution,'/return/<int:id>/<int:idprod>/<int:idcolor>/<int:idsize>')
+
+class UploadDevolution(Resource):
+    @ns_upload.response(HTTPStatus.OK.value,"Realiza envio de arquivo(s) para o servidor na pasta de importação")
+    @ns_upload.response(HTTPStatus.BAD_REQUEST.value,"Falha ao enviar arquivo(s)!")
+    @auth.login_required
+    def post(self):
+        try:
+            type = request.args.get("type")
+            files = []
+            #obtem os arquivos para upload
+            fpath = os.environ.get("F2B_APP_PATH")+'assets/import/'
+            data = ImmutableMultiDict(request.files)
+            for file in data.getlist('files[]'):
+                parts = file.filename.split(".")
+                ext = parts[len(parts)-1]
+                newFileName = "import_"+str(type)+datetime.now().strftime("%Y%m%d-%H%M%S")+"."+ext
+                file.save(fpath+newFileName)
+                file.close()
+                files.append(newFileName)
+            return files
+        except exceptions.HTTPException as e:
+            print(e)
+            return False
+ns_upload.add_resource(UploadDevolution,'/import/')
